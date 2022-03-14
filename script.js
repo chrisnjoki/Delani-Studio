d35feca8a87f4ddb841ace0fd061122802aed7b7
kris <cmuchiri810@gmail.com>
2022-03-14 16:13:20 +0300

    modified js/html/css

--- script.js
+++ script.js
@@ -19,4 +19,26 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $(".1-image").mouseenter(function(){
+        $(".h6a").show();
+        $("1-image").css("opacity","0.9")
+    });
 
+    $(".1-image").mouseleave(function(){
+        $(".h6a").hide();
+        $("1-image").css("opacity","2")
+    });
+});
+
+$(document).ready(function(){
+    $(".2-image").mouseenter(function(){
+        $(".h6b").show();
+        $("1-image").css("opacity","0.9")
+    });
+
+    $(".2-image").mouseleave(function(){
+        $(".h6b").hide();
+        $("2-image").css("opacity","2")
+    });
+});
