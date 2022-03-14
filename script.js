e8d9cee3dcab9b76f91a4f94fc1b54e85be873c8
kris <cmuchiri810@gmail.com>
2022-03-14 23:15:50 +0300

    modified html/css/js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".2-image").mouseenter(function(){
         $(".h6b").show();
-        $("1-image").css("opacity","0.9")
+        $("2-image").css("opacity","0.9")
     });
 
     $(".2-image").mouseleave(function(){
@@ -42,3 +42,78 @@ $(document).ready(function(){
         $("2-image").css("opacity","2")
     });
 });
+
+$(document).ready(function(){
+    $(".3-image").mouseenter(function(){
+        $(".h6c").show();
+        $("3-image").css("opacity","0.9")
+    });
+
+    $(".3-image").mouseleave(function(){
+        $(".h6c").hide();
+        $("3-image").css("opacity","2")
+    });
+});
+
+$(document).ready(function(){
+    $(".4-image").mouseenter(function(){
+        $(".h6d").show();
+        $("4-image").css("opacity","0.9")
+    });
+
+    $(".4-image").mouseleave(function(){
+        $(".h6d").hide();
+        $("4-image").css("opacity","2")
+    });
+});
+
+$(document).ready(function(){
+    $(".5-image").mouseenter(function(){
+        $(".h6e").show();
+        $("5-image").css("opacity","0.9")
+    });
+
+    $(".5-image").mouseleave(function(){
+        $(".h6e").hide();
+        $("5-image").css("opacity","2")
+    });
+});
+
+$(document).ready(function(){
+    $(".6-image").mouseenter(function(){
+        $(".h6f").show();
+        $("6-image").css("opacity","0.9")
+    });
+
+    $(".6-image").mouseleave(function(){
+        $(".h6f").hide();
+        $("6-image").css("opacity","2")
+    });
+});
+
+$(document).ready(function(){
+    $(".7-image").mouseenter(function(){
+        $(".h6g").show();
+        $("7-image").css("opacity","0.9")
+    });
+
+    $(".7-image").mouseleave(function(){
+        $(".h6g").hide();
+        $("7-image").css("opacity","2")
+    });
+});
+
+$(document).ready(function(){
+    $(".8-image").mouseenter(function(){
+        $(".h6h").show();
+        $("8-image").css("opacity","0.9")
+    });
+
+    $(".8-image").mouseleave(function(){
+        $(".h6h").hide();
+        $("8-image").css("opacity","2")
+    });
+});
+
+
+
